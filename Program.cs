41952764dbc11e10b68b256bfe714d5c27b6dd0b
adfeswargv <aerthsertsrtrstb@ebebsbb.com>
2024-05-06 15:40:52 -0400

    Feat: Added all endpoints and async functions onto program.cs

--- Program.cs
+++ Program.cs
@@ -15,10 +15,54 @@ var chairs = app.MapGroup("api/chair");
 //TODO: ASIGNACION DE RUTAS A LOS ENDPOINTS
 chairs.MapGet("/", GetChairs);
 
+chairs.MapPost("/", AddChair);
+
+chairs.MapGet("/{name}", GetChairByName);
+
+chairs.MapPut("/{id}", UpdateChair);
+
+chairs.MapPut("/{id}/stock", IncrementChairStock);
+
+chairs.MapPost("/purchase", BuyChair);
+
+chairs.MapDelete("/{id}", DeleteChairById);
+
 app.Run();
 
 //TODO: ENDPOINTS SOLICITADOS
-static IResult GetChairs(DataContext db)
+static async Task<IResult> GetChairs(DataContext db)
+{
+    return TypedResults.Ok();
+}
+
+static async Task<IResult> AddChair(Chair chair, DataContext db)
+{
+    db.Chairs.Add(chair);
+    await db.SaveChangesAsync();
+    return TypedResults.Created($"/chairs/{chair.Id}", chair);
+}
+
+static async Task<IResult> GetChairByName(String name, DataContext db)
+{
+    return TypedResults.Ok();
+}
+
+static async Task<IResult> UpdateChair(int id, DataContext db)
+{
+    return TypedResults.Ok();
+}
+
+static async Task<IResult> IncrementChairStock(int id, DataContext db)
+{
+    return TypedResults.Ok();
+}
+
+static async Task<IResult> BuyChair(int id, int ammount, DataContext db)
+{
+    return TypedResults.Ok();
+}
+
+static async Task<IResult> DeleteChairById(int id, DataContext db)
 {
     return TypedResults.Ok();
 }
